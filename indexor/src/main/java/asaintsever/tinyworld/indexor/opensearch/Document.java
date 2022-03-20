564f326e0be274a1aa84133e00699ceb5caf0312
Alain Saint-Sever <alstsever@free.fr>
2022-03-20 19:50:35 +0100

    Add document search and tests

--- indexor/src/main/java/asaintsever/tinyworld/indexor/opensearch/Document.java
+++ indexor/src/main/java/asaintsever/tinyworld/indexor/opensearch/Document.java
@@ -1,33 +1,126 @@
 package asaintsever.tinyworld.indexor.opensearch;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.action.search.SearchRequest;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.opensearch._global.CountRequest;
+import org.opensearch.client.opensearch._global.CountResponse;
+import org.opensearch.client.opensearch._global.GetRequest;
+import org.opensearch.client.opensearch._global.GetResponse;
 import org.opensearch.client.opensearch._global.IndexRequest;
 import org.opensearch.client.opensearch._global.IndexResponse;
-import org.opensearch.client.opensearch._global.SearchResponse;
+import org.opensearch.index.query.QueryBuilder;
+import org.opensearch.index.query.QueryBuilders;
+import org.opensearch.search.SearchHit;
+import org.opensearch.search.SearchHits;
+import org.opensearch.search.builder.SearchSourceBuilder;
 
-public class Document<T> {
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import asaintsever.tinyworld.indexor.IndexPage;
+
+
+public class Document<T> implements Closeable {
     
-    private OpenSearchClient osClient;
+    private ObjectMapper mapper;
+    private ClusterClient client;
     private String index;
-
-    public void setClient(OpenSearchClient client) {
-        this.osClient = client;
+    
+    // Used for search only (new Java client search capabilities not on par compared to high-level rest client)
+    // To be removed once new Java client fully supports all search expressions
+    private RestHighLevelClient restHlClient;
+    
+    
+    public Document(ClusterClient client) {
+        this.setClient(client);
+    }
+    
+    // Getter to allow customization of mapper
+    public ObjectMapper getMapper() {
+        return this.mapper;
     }
 
-    public void setIndex(String index) {
+    public Document<T> setClient(ClusterClient client) {
+        this.client = client;
+        this.mapper = client.getMapper();
+        
+        // Create internal high-level rest client in addition (for advanced search only)
+        this.restHlClient = new RestHighLevelClient(client.getClientBuilder());
+        return this;
+    }
+    
+    public Document<T> setIndex(String index) {
         this.index = index;
+        return this;
     }
 
     public String add(T document) throws IOException {
         IndexRequest<T> indexRequest = new IndexRequest.Builder<T>().index(this.index).value(document).build();
-        IndexResponse indexResponse = this.osClient.index(indexRequest);
+        IndexResponse indexResponse = this.client.getClient().index(indexRequest);
         return indexResponse.id();
     }
+    
+    public T get(String id, Class<T> docClass) throws IOException {       
+        GetRequest getRequest = new GetRequest.Builder().index(this.index).id(id).build();
+        GetResponse<T> getResponse = this.client.getClient().get(getRequest, docClass);
+        return getResponse.source();
+    }
+    
+    public long count() throws IOException {
+        CountRequest countRequest = new CountRequest.Builder().index(this.index).build();
+        CountResponse countResponse = this.client.getClient().count(countRequest);
+        return countResponse.count().longValue();
+    }
+    
+    public IndexPage<T> search(String queryDSL, Class<T> docClass) throws IOException {
+        return this.search(queryDSL, 0, 10, docClass);
+    }
+    
+    public IndexPage<T> search(String queryDSL, int from, int size, Class<T> docClass) throws IOException {
+        QueryBuilder searchQuery = QueryBuilders.wrapperQuery(queryDSL);
+        
+        SearchRequest searchRequest = new SearchRequest(this.index);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.trackTotalHits(true).from(from).size(size).query(searchQuery);
+        searchRequest.source(searchSourceBuilder);
 
-    public SearchResponse<T> search() {
-        // TODO Auto-generated method stub
-        return null;
+        SearchResponse searchResponse = this.restHlClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = searchResponse.getHits();
+        
+        if (hits.getTotalHits().value == 0 || hits.getHits().length == 0) {
+            return IndexPage.EMPTY;
+        }
+        
+        SearchHit[] searchHits = hits.getHits();
+        List<T> resultList = new ArrayList<T>();
+        
+        for (SearchHit hit : searchHits) {
+            // Try to unserialize source to object using provided class
+            T result = mapper.readValue(hit.getSourceAsString(), docClass);
+            resultList.add(result);
+        }
+
+        return new IndexPage<T>(resultList, queryDSL, hits.getTotalHits().value, searchSourceBuilder.from(), searchSourceBuilder.size());
+    }
+    
+    
+    /**
+     * By default, you cannot use from and size to page through more than 10,000 hits. This limit is a safeguard set by the index.max_result_window index setting.
+     * If you need to page through more than 10,000 hits, use the search_after parameter instead.
+     * See https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
+     */
+    public IndexPage<T> next(IndexPage<T> page, Class<T> docClass) throws IOException {
+        return this.search(page.query(), page.from() + page.size(), page.size(), docClass);
+    }
+
+    @Override
+    public void close() throws IOException {
+        if(this.restHlClient != null) this.restHlClient.close();  // do not forget to free the internal high-level rest client
     }
 }
--- indexor/src/test/java/asaintsever/tinyworld/indexor/opensearch/ClusterClientTest.java
+++ indexor/src/test/java/asaintsever/tinyworld/indexor/opensearch/ClusterClientTest.java
@@ -1,29 +1,55 @@
 package asaintsever.tinyworld.indexor.opensearch;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.opensearch.client.ResponseException;
 import org.opensearch.rest.RestStatus;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import asaintsever.tinyworld.indexor.IndexPage;
 import asaintsever.tinyworld.indexor.opensearch.Cluster.ClusterNode;
 import asaintsever.tinyworld.indexor.opensearch.Cluster.ClusterNodeException;
+import asaintsever.tinyworld.metadata.extractor.CustomDateSerializer;
+
 
 public class ClusterClientTest {
     
     private static ClusterNode node;
     private static ClusterClient client;
+    private static long seed;
     
+    public static class DocObject {
+        public String attr1;
+        public String attr2;
+        
+        @JsonSerialize(using = CustomDateSerializer.class)
+        public Date attr3;
+        
+        @Override
+        public String toString() {
+            return "[attr1:" + this.attr1 + ", attr2:" + this.attr2 + ", attr3:" + this.attr3.toString() + "]";
+        }
+    }
+        
     
     @BeforeAll
     public static void setup() throws ClusterNodeException {
         // Create single node cluster
-        node = new Cluster().setPathHome("target/index").create(false);
+        node = new Cluster().setPathHome("target/index").create(true);
         
         // Create client
         client = new ClusterClient("localhost", 9200);
@@ -35,15 +61,21 @@ public class ClusterClientTest {
         node.close();
     }
     
+    @BeforeEach
+    void setupTest() {
+        // Get new random seed to init our EasyRandom instance
+        seed = new Random().nextLong();
+    }
+    
     @AfterEach
-    void teardown() throws IOException {
+    void teardownTest() throws IOException {
         client.deleteIndex("*");
     }
     
     
     @Test
     void createThenDeleteIndex() throws IOException {
-        assertTrue(client.createIndex("test.index"));
+        assertTrue(client.createIndex("test.index", null));
         assertTrue(client.isIndexExists("test.index"));
         
         assertTrue(client.deleteIndex("test.index"));
@@ -52,12 +84,12 @@ public class ClusterClientTest {
     
     @Test
     void createIndexTwice() throws IOException {
-        assertTrue(client.createIndex("test.index"));
+        assertTrue(client.createIndex("test.index", null));
         assertTrue(client.isIndexExists("test.index"));
         
         assertEquals(
                 assertThrows(ResponseException.class, () -> {
-                    client.createIndex("test.index");
+                    client.createIndex("test.index", null);
                 })
                 .getResponse().getStatusLine().getStatusCode(), 
                 RestStatus.BAD_REQUEST.getStatus());
@@ -71,5 +103,94 @@ public class ClusterClientTest {
                 })
                 .getResponse().getStatusLine().getStatusCode(), 
                 RestStatus.NOT_FOUND.getStatus());
+    }
+    
+    @Test
+    void insertCountAndGetDocument() throws IOException, InterruptedException {
+        assertFalse(client.isIndexExists("test.index"));
+                       
+        try(Document<DocObject> doc = new Document<>(client)) {
+            doc.setIndex("test.index").getMapper().setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            
+            // No mapping provided: our attr3 date field will be stored using text type
+            String id = doc.add(new EasyRandom(new EasyRandomParameters().seed(seed)).nextObject(DocObject.class));
+            assertTrue((id != null) && !id.isEmpty());
+            
+            // Index is created automatically if not exist when inserting documents
+            assertTrue(client.isIndexExists("test.index"));
+            
+            // Pause before asking # of doc in index
+            Thread.sleep(2000);
+            assertEquals(doc.count(), 1);
+            
+            DocObject docObj = doc.get(id, DocObject.class);
+            System.out.println("Document=" + docObj.toString());
+        }
+    }
+    
+    @Test
+    void insertThenSearchDocuments() throws IOException, InterruptedException {
+        assertFalse(client.isIndexExists("test.index"));
+        
+        // Set explicit mapping so that dates are handled the way we want
+        String mapping = ""
+                + "{\n"
+                + " \"properties\": {\n"
+                + "   \"attr1\": {\n"
+                + "     \"type\": \"text\"\n"
+                + "   },\n"
+                + "   \"attr2\": {\n"
+                + "     \"type\": \"text\"\n"
+                + "   },\n"
+                + "   \"attr3\": {\n"
+                + "     \"type\": \"date\",\n"
+                + "     \"format\": \"yyyy-MM-dd HH:mm:ss\"\n"
+                + "   }\n"
+                + " }\n"
+                + "}";
+        
+        assertTrue(client.createIndex("test.index", mapping));
+        
+        try(Document<DocObject> doc = new Document<>(client)) {
+            // Set date format for your Document mapper to match defined format for DocObject
+            doc.setIndex("test.index").getMapper().setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            
+            EasyRandomParameters parameters = new EasyRandomParameters().seed(seed).dateRange(LocalDate.of(2021, 12, 1), LocalDate.of(2022, 1, 10));
+            EasyRandom easyRandom = new EasyRandom(parameters);
+            
+            for(int i=0;i<10;i++) {
+                assertTrue(() -> {
+                    try {
+                        // Insert documents
+                        String id = doc.add(easyRandom.nextObject(DocObject.class));
+                        return ((id != null) && !id.isEmpty());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+            
+            // Pause before asking # of doc in index
+            Thread.sleep(2000);
+            assertEquals(doc.count(), 10);
+            
+            // Search all documents
+            IndexPage<DocObject> docObjList = doc.search("{\"simple_query_string\": {\"query\": \"*\"}}", DocObject.class);
+            System.out.println("Total=" + docObjList.total() + ", Size=" + docObjList.size() + ", Result=" + docObjList.get().toString());
+            
+            // Search for documents with attr3 date before 2022
+            String queryDSL = ""
+                    + "{\n"
+                    + " \"range\": {\n"
+                    + "   \"attr3\": {\n"
+                    + "      \"lt\": \"2022-01-01\",\n"
+                    + "      \"format\": \"yyyy-MM-dd\"\n"
+                    + "   }\n"
+                    + " }\n"
+                    + "}";       
+            
+            docObjList = doc.search(queryDSL, 0, 5, DocObject.class);
+            System.out.println("Total=" + docObjList.total() + ", Size=" + docObjList.size() + ", Result=" + docObjList.get().toString());
+        }
     }
 }
