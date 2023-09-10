4aecf563e0f6a0c5684a225867ca0b5cc0b733dd
TheAlain <43777839+asaintsever@users.noreply.github.com>
2023-09-10 18:46:21 +0200

    Intellij migration & Chromebook support (arm64) (#1)
    * Update JOGL libs
    * Get rid of DDS
    * Override default BMNGOneImage
    * Fix opensearch cluster network exposure
    * Add target to use OpenGL sw renderer
    * Update release scripts

--- release/get-3rd-party.sh
+++ release/get-3rd-party.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+echo "Get 3rd-party software ..."
+
 # clean
 rm -rf release/tmp/3rd || true
 
-mkdir -p release/tmp/3rd/linux
-mkdir -p release/tmp/3rd/windows
+mkdir -p release/tmp/3rd
 
 # Generate 3rd-party license
 touch "release/tmp/3rd/LICENSE-3RD-PARTY.txt"
@@ -14,7 +15,8 @@ echo -e "\n\n=====\nLicense for ImageMagick <https://imagemagick.org/>\n=====\n\
 curl -s -L https://raw.githubusercontent.com/asaintsever/tinyworld-utils/main/ImageMagick-LICENSE >> "release/tmp/3rd/LICENSE-3RD-PARTY.txt"
 
 # Get 3rd-party software
-curl -s -L --output release/tmp/3rd/linux/magick https://github.com/asaintsever/tinyworld-utils/releases/download/imagemagick-heic/magick
-chmod +x release/tmp/3rd/linux/magick
+curl -s -L --output release/tmp/3rd/magick-linux-x86_64 https://github.com/asaintsever/tinyworld-utils/releases/download/imagemagick-heic/magick-linux-x86_64
+curl -s -L --output release/tmp/3rd/magick-linux-aarch64 https://github.com/asaintsever/tinyworld-utils/releases/download/imagemagick-heic/magick-linux-aarch64
+chmod +x release/tmp/3rd/magick-linux-*
 
-curl -s -L --output release/tmp/3rd/windows/magick.exe https://github.com/asaintsever/tinyworld-utils/releases/download/imagemagick-heic/magick.exe
+curl -s -L --output release/tmp/3rd/magick-windows-x86_64.exe https://github.com/asaintsever/tinyworld-utils/releases/download/imagemagick-heic/magick-windows-x86_64.exe
